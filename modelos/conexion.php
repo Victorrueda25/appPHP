83c378ebe6e8118d86509df2ab6b3fe22806130b
Victorrueda25 <victor_skyes@hotmail.com>
2025-04-30 19:19:20 -0500

    Base de datos, arreglo de inicio

---
+++ modelos/conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Conexion{
+
+        static public function conectar(){
+
+            $link = new PDO("mysql:host=localhost:3307;dbname=php_sena","root","");
+            return $link;
+
+        }
+
+    }
